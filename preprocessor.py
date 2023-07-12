200234a5640b0dbdbf23b3747ae33307eb5d2ff6
Anthony Zhang <anthonyzhang@squareup.com>
2023-07-12 16:37:44 -0700

    added comments describing what's going on

--- preprocessor.py
+++ preprocessor.py
@@ -1,10 +1,12 @@
 from bs4 import BeautifulSoup
 import tiktoken
 
+# outputs the token length of a string according to the "cl100k_base" model
 def tokenLength(input):
     encoding = tiktoken.get_encoding("cl100k_base")
     return len(encoding.encode(input))
 
+# cleans up all images and whitespace from an HTML, then outputs a list where each entry is a text paragraph
 def extract_paragraphs_from_html(html_file):
     with open(html_file, 'r') as file:
         soup = BeautifulSoup(file, 'html.parser')
@@ -15,6 +17,7 @@ def extract_paragraphs_from_html(html_file):
                 paragraphs.append(text)
         return paragraphs
 
+# groups the paragraphs into tokens of length 256 or less
 def grouper(paragraphs):
     output = []
     currentSegment = paragraphs[0]
@@ -29,6 +32,7 @@ def grouper(paragraphs):
         i += 1
     return output
 
+# testing
 if __name__ == "__main__":
     ledger_filepath = "/Users/anthonyzhang/Development/square-hack-week/LedgerFAQ_goledgerfaq_.html"
     paragraphs = extract_paragraphs_from_html("/Users/anthonyzhang/Development/square-hack-week/LedgerFAQ_goledgerfaq_.html")
